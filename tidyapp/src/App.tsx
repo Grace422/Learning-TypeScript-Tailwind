2339214e835177a661811077f975c207ae185306
Grace <neguekwahammaelgrace@gmail.com>
2025-01-22 09:47:42 +0100

    fixed div for dynamic content

--- tidyapp/src/App.tsx
+++ tidyapp/src/App.tsx
@@ -29,14 +29,14 @@ function App() {
 
   return (
     <div className="h-full w-full pt-40">
-      <div className="max-w-xl mx-auto flex flex-col items-center ml-8 mr-8 sm:max-w-xl sm:mx-auto sm:flex sm:flex-col sm:items-center">
+      <div className="max-w-xl mx-auto flex flex-col items-center ml-5 mr-5 sm:max-w-xl sm:mx-auto sm:flex sm:flex-col sm:items-center">
         <FolderCheck size={64} />
         <h1 className="font-bold text-5xl">Tidyup</h1>
         <p className="text-neutral-800 text-lg text-center mt-5">
           A simple CLI tool to organize your folders and files.
         </p>
-        <div className="space-x-1 mt-3 flex flex-col justify-center sm:space-x-4 sm:flex-row sm:justify-center">
-          <button className="py-2 px-8 bg-neutral-900 rounded-md text-white hover:bg-neutral-800 sm:py-1 sm:px-8 sm:bg-neutral-900 sm:rounded-md sm:text-white sm:hover:bg-neutral-800">
+        <div className="space-x-2 mt-3 sm:space-x-4 flex flex-row justify-center">
+          <button className="py-1 px-8 bg-neutral-900 rounded-md text-white hover:bg-neutral-800 sm:py-1 sm:px-8 sm:bg-neutral-900 sm:rounded-md sm:text-white sm:hover:bg-neutral-800">
             Npm Package
           </button>
           <button className="py-2 px-8 border-2 rounded-md hover:bg-gray-100 mt-2 sm:py-2 sm:px-8 sm:border-2 sm:rounded-md sm:hover:bg-gray-100">Github</button>
@@ -58,7 +58,7 @@ function App() {
 
           <div className="flex flex-col gap-2">
             <h2 className="text-xl font-semibold">Usage</h2>
-            <div className="border rounded-md bg-gradient px-4 py-4 bg-gray-50">
+            <div className="border rounded-md bg-gradient px-4 py-4 bg-gray-55 min-h-[300px] max-h-[400px] w-full overflow-y-auto overflow-x-hidden">
               <div className="flex items-center space-x-1 mb-4">
                 <div className="w-3 h-3 rounded-full bg-red-500" />
                 <div className="w-3 h-3 rounded-full bg-yellow-500" />
@@ -112,19 +112,19 @@ function App() {
               </ul>
             </div>
 
-            <div className="border-t py-8 mt-40 mx-auto max-w-xl w-full items-center flex justify-between">
+            <div className="border-t py-8 mt-40 mx-auto max-w-xl w-full items-center flex flex-col sm:border-t sm:py-8 sm:mt-40 sm:mx-auto sm:max-w-xl sm:w-full sm:items-center sm:flex sm:flex-row sm:justify-between">
               <p className="flex items-center space-x-2 ">
                 <img
                   src={img}
                   alt="woman image"
-                  className="rounded-full size-10 mr-2"
+                  className="rounded-full mr-2 overflow-hidden h-9 w-10 sm:h-10 sm:w-15 sm:rounded-full sm:mr-2 sm:overflow-hidden"
                 />
                 by
                 <a href="" className="font-bold">
                   Kwaham Grace
                 </a>
               </p>
-              <p>
+              <p className="mt-5 sm:mt-0">
                 Design inspired by{" "}
                 <a href="" className="font-bold">
                   Sonner
